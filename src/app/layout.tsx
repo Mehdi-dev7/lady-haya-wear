103c7630e831ce9cdd7dd2b09f34edad0fdd8055
Wil-dev <mehdididou7@icloud.com>
2025-07-18 18:00:10 +0200

    Modification du délai de fermeture automatique du ToastContainer dans layout.tsx, passant de 5000 à 3000 ms pour une meilleure réactivité des notifications.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 							<StudioWrapper>{children}</StudioWrapper>
 							<ToastContainer
 								position="top-right"
-								autoClose={5000}
+								autoClose={3000}
 								hideProgressBar={false}
 								newestOnTop={false}
 								closeOnClick
